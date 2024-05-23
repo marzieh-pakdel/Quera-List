f480a9f780473bdc52c23ced2b96073aca56d59a
Marziye <pakdel.marzieh1380@gmail.com>
2024-05-24 00:49:18 +0330

    install tailwind with npm

---
+++ tailwind.config.js
@@ -0,0 +1,73 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2C7EF8",
+        "primary-hover-light": "#075CD9",
+        "on-primary-light": "#FFFFFF",
+        "background-light": "#EBEDEF",
+        "on-background-light": "#191C1E",
+        "surface-light": "#EBEDEF",
+        errors: "#E63946",
+        success: "#90F677",
+        "neutral-1": "#000000",
+        "neutral-2": "#323233",
+        "neutral-3": "#646466",
+        "neutral-4": "#7D7D7F",
+        "neutral-5": "#AFAEB2",
+        "neutral-6": "#C8C7CB",
+        "neutral-7": "#E1E0E5",
+        "neutral-8": "#E1E0E5",
+        "neutral-9": "#F5F6F8",
+        "neutral-10": "#FEFEFE",
+        "neutral-11": "#FFFFFF",
+        border: "#E3E6E8",
+        "dark-gray": "#666666",
+        "light-text": "#444749",
+        comment: "#5C5C5C",
+        "dark-text": "#242424",
+        "input-border": "#A1A1A1",
+        placeholder: "#BCBCBC",
+      },
+
+      screens: {
+        lg: "360px",
+      },
+
+      fontFamily: {
+        "Yekan-bakh": "YekanBakh",
+      },
+
+      fontWeight: {
+        medium: "900",
+        bold: "700",
+        semibold: "600",
+        regular: "400",
+        light: "300",
+      },
+
+      fontSize: {
+        10: "0.625rem",
+        11: "0.688rem",
+        12: "0.75rem",
+        13: "0.813rem",
+        14: "0.875rem",
+        15: "0.938rem",
+        16: "1rem",
+        18: "1.125rem",
+        20: "1.25rem",
+        24: "1.5rem",
+        28: "1.75rem",
+        32: "2rem",
+        36: "2.25rem",
+        40: "2.5rem",
+        42: "2.625rem",
+        46: "2.875rem",
+        64: "4rem",
+      },
+    },
+  },
+}
+
